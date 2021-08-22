39317432bdc14fd6704e2d0729ba9233323039c6
ElSeoClub <elseoclub@gmail.com>
2021-08-22 18:35:12 -0500

    Se modifica la pantalla de inicio de sesión

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,55 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
-
-        <x-jet-validation-errors class="mb-4" />
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+<x-login-layout>
+    <section class="min-h-screen flex items-stretch text-white">
+        <div class="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center z-0"
+            style="background-image: url({{asset('img/login_background.jpg')}});">
+            <div class="clouds z-10"></div>
+            <div class="w-full px-24 z-20">
+                <h1 class="text-5xl font-bold text-left tracking-wide">Sistema de legitimación</h1>
+                <p class="text-3xl my-4">Sindicato Único de trabajadores Electricistas de la República Mexicana</p>
+                <p class="text-lg my-4">"Por la transformación de México"</p>
             </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="bottom-0 absolute p-4 text-center right-0 left-0 flex justify-center space-x-4 z-20">
+                <a href="https://twitter.com/suterm_nacional" target="_blank">
+                    <i class="fab fa-twitter text-2xl"></i>
+                </a>
+                <a href="https://www.facebook.com/SutermNacional" target="_blank">
+                    <i class="fab fa-facebook-f text-2xl"></i>
+                </a>
+                <a href="https://suterm.mx" target="_blank">
+                    <i class="fas fa-home text-2xl"></i>
+                </a>
             </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+        </div>
+        <div class="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0"
+            style="background-color: #161616;">
+            <div class="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center"
+                style="background-image: url({{asset('img/login_background.jpg')}});">
+                <div class="absolute stars z-0 inset-0"></div>
+                <div class="clouds"></div>
             </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+            <div class="w-full py-6 z-20">
+                <h1 class="my-6">
+                    <img class="m-auto" src="{{asset('svg/suterm.png')}}" />
+                    <img class="m-auto h-12 mt-2" src="{{asset('svg/logo_name.svg')}}" />
+                </h1>
+                <form method="post" action="{{route('login')}}" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+                    @csrf
+                    <div class="pb-2 pt-4">
+                        <input type="text" name="username" id="username" placeholder="{{__("Username")}}"
+                            value="{{old('username')}}" class="block w-full p-4 text-lg rounded-sm bg-black" required
+                            autofocus autocomplete="off">
+                    </div>
+                    <div class="pb-2 pt-4">
+                        <input class="block w-full p-4 text-lg rounded-sm bg-black" type="password" name="password"
+                            id="password" placeholder="{{__('Password')}}" required autocomplete="current-password">
+                        <div class="mt-3">{{$errors->first('username')}}</div>
+                    </div>
+                    <div class="px-4 pb-2 pt-4">
+                        <button
+                            class="uppercase block w-full p-4 text-lg rounded-full bg-red-500 hover:bg-red-600 focus:outline-none">{{__('Sign In')}}</button>
+                    </div>
+                </form>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+    </section>
+</x-login-layout>
