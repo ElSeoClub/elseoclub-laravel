4f9c57e3c85e780d393591bf49d2782b6ae65f84
ElSeoClub <elseoclub@gmail.com>
2021-08-22 06:56:01 -0500

    Se crea grupo de rutas exclusivas de administrador

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->group(function () {
+    Route::get('/usuarios', [UsersController::class, 'index'])->name('users.index');
+});
