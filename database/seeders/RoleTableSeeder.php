348a6171b3a0dcd7d716b71034648af5f1c32bfd
ElSeoClub <elseoclub@gmail.com>
2021-08-22 05:46:57 -0500

    Se agregan semillas para usuario admin

---
+++ database/seeders/RoleTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = new Role();
+        $role->name = 'Administrador';
+        $role->description = 'Perfil de control total del sistema, destinado únicamente a TI.';
+        $role->save();
+    }
+}
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = "Ricardo";
+        $user->email = 'suvager@gmail.com';
+        $user->password = '$2y$10$1qXs2VzJzyNxFDzlNFPml.MxYmXy/unXDmpEI8C/IaMgbFn3iq7nS';
+        $user->save();
+
+        $user->roles()->attach(Role::where('name', 'Administrador')->first());
+    }
+}
