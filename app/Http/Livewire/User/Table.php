6a45fef33f399cdcd4d69af156e827a8e5e05a08
ElSeoClub <elseoclub@gmail.com>
2021-08-22 19:04:12 -0500

    Se agrega busqueda por username, name, email

--- app/Http/Livewire/User/Table.php
+++ app/Http/Livewire/User/Table.php
@@ -20,7 +20,7 @@ class Table extends Component
 
     public function render()
     {
-        $users = User::where('name', 'like', '%' . $this->search . '%')->paginate(10);
+        $users = User::where('name', 'like', '%' . $this->search . '%')->orwhere('username', 'like', '%' . $this->search . '%')->orwhere('email', 'like', '%' . $this->search . '%')->paginate(10);
         return view('livewire.user.table', compact('users'));
     }
 }
