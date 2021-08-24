1927514e1be585f4b66f3a208fc663a564c7deb8
ElSeoClub <elseoclub@gmail.com>
2021-08-24 09:45:16 -0500

    Se crea componente de boton

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,22 @@
+@props(['class' => ''])
 <div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+    @if ($href != null)
+    <a href={{$href}}>
+        <button type="{{$type}}" {{$click != null ? 'wire:click='.$click.'':''}} {{ $attributes->merge(['class' => 'text-base bg-'.$color.'-500 text-white font-bold px-3 py-1 rounded
+            hover:bg-'.$color.'-600 focus:bg-'.$color.'-600 focus:outline-none focus:ring focus:ring-'.$color.'-600
+            focus:ring-opacity-50 '.$class]) }}> @if ($icon !=null) <i class="{{$icon}} mr-2"></i>
+            @endif
+            {{$slot}}
+        </button>
+    </a>
+    @else
+    <button type="{{$type}}" {{$click != null ? 'wire:click='.$click.'':''}} {{ $attributes->merge(['class' => 'text-base bg-'.$color.'-500 text-white font-bold px-3 py-1 rounded
+    hover:bg-'.$color.'-600 focus:bg-'.$color.'-600 focus:outline-none focus:ring focus:ring-'.$color.'-600
+    focus:ring-opacity-50 '.$class]) }}>
+        @if ($icon != null)
+        <i class="{{$icon}} mr-2"></i>
+        @endif
+        {{$slot}}
+    </button>
+    @endif
 </div>
