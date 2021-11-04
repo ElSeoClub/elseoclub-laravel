cb2ace3aa9a7c072b1337c5dd9058f7c5fa659bf
ElSeoClub <elseoclub@gmail.com>
2021-11-03 18:49:57 -0600

    test

--- resources/views/livewire/event/legitimation/location/index.blade.php
+++ resources/views/livewire/event/legitimation/location/index.blade.php
@@ -1,27 +1,25 @@
 <div class="grid grid-cols-5 gap-5">
     <div class="col-span-3">
-        <x-card title="Sedes del evento" px="0" py="0">
-            <x-table>
-                <x-slot name="thead">
-                    <th class="text-left px-5 py-1">Nombre</th>
-                    <th class="text-left px-5 py-1"># de invitados</th>
-                    <th class="text-left px-5 py-1"># de Boletas</th>
-                </x-slot>
-                <x-slot name="tbody">
-                    @foreach ($locations as $key => $location)
-                    @if (Auth::user()->hasPermission($location->name))
-                    <tr class="hover:bg-gray-100">
-                        <td class="px-5 py-1">{{$location->name}}</td>
-                        <td class="px-5 py-1">{{$location->guests()->count()}}</td>
-                        <td class="px-5 py-1"><input type="text" wire:keyup="save({{$location->id}},event.target.value)"
-                                value="{{$location->boletas}}"></td>
-                    </tr>
-                    @endif
+        <x-table>
+            <x-slot name="thead">
+                <th class="text-left px-5 py-1">Nombre</th>
+                <th class="text-left px-5 py-1"># de invitados</th>
+                <th class="text-left px-5 py-1"># de Boletas</th>
+            </x-slot>
+            <x-slot name="tbody">
+                @foreach ($locations as $key => $location)
+                @if (Auth::user()->hasPermission($location->name))
+                <tr class="hover:bg-gray-100">
+                    <td class="px-5 py-1">{{$location->name}}</td>
+                    <td class="px-5 py-1">{{$location->guests()->count()}}</td>
+                    <td class="px-5 py-1"><input type="text" wire:keyup="save({{$location->id}},event.target.value)"
+                            value="{{$location->boletas}}"></td>
+                </tr>
+                @endif
 
-                    @endforeach
-                </x-slot>
-            </x-table>
-        </x-card>
+                @endforeach
+            </x-slot>
+        </x-table>
     </div>
     <div class="col-span-2">
         <x-card title="Asistencia" px="0" py="0">
