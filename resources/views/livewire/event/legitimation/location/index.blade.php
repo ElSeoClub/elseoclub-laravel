4b0331189790626e572b74bc97aac4293b37ef16
ElSeoClub <elseoclub@gmail.com>
2021-11-03 18:55:36 -0600

    test

--- resources/views/livewire/event/legitimation/location/index.blade.php
+++ resources/views/livewire/event/legitimation/location/index.blade.php
@@ -4,7 +4,7 @@
             <x-slot name="thead">
                 <th class="text-left px-5 py-1">Nombre</th>
                 <th class="text-left px-5 py-1"># de invitados</th>
-                <th class="text-left px-5 py-1"># de Boletas</th>
+                <th class="text-left px-5 py-1"># asistentes</th>
             </x-slot>
             <x-slot name="tbody">
                 @foreach ($locations as $key => $location)
@@ -29,30 +29,21 @@
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Red'],
         datasets: [{
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
     },
     options: {
+        indexAxis: 'y',
         scales: {
             y: {
                 beginAtZero: true
