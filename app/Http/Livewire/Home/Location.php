0b56e55a7df1739af3bdd87cd15358f2c356c4e2
ElSeoClub <elseoclub@gmail.com>
2021-11-16 20:00:26 -0600

    test

--- app/Http/Livewire/Home/Location.php
+++ app/Http/Livewire/Home/Location.php
@@ -3,14 +3,21 @@
 namespace App\Http\Livewire\Home;
 
 use Livewire\Component;
+use App\Models\Event;
+use App\Models\User;
 
 class Location extends Component
 {
     public $location = false;
+    public $user;
+    public $username;
+    public $event;
 
     public function display_location()
     {
         $this->location = true;
+        $this->event = Event::find(1);
+        $this->user = User::where('username', $this->username)->first();
     }
 
     public function render()
