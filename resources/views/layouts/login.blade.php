6c86f1d419a06ca41cc975d5289b97603a298231
ElSeoClub <elseoclub@gmail.com>
2021-11-19 20:29:01 -0600

    test

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Sistema de legitimación - Siconecta') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
