569f409889ce945de7ac550d106201c0a9200517
ElSeoClub <elseoclub@gmail.com>
2021-11-28 21:15:21 -0600

    buscar por nombre o rpe

--- app/Http/Livewire/Event/Legitimation/Guests.php
+++ app/Http/Livewire/Event/Legitimation/Guests.php
@@ -104,7 +104,7 @@ class Guests extends Component
 
     public function render()
     {
-        $user_list = $this->event->guests()->where('username', 'like', "%$this->search%")->paginate(20);
+        $user_list = $this->event->guests()->where('username', 'like', "%$this->search%")->orwhere('name', 'like', "%$this->search%")->paginate(20);
         return view('livewire.event.legitimation.guests', compact('user_list'));
     }
 }
