a0e77e41e135943718e3019f4460eed92574c258
ElSeoClub <elseoclub@gmail.com>
2021-12-01 13:44:30 -0600

    test

--- resources/views/event/legitimation/show.blade.php
+++ resources/views/event/legitimation/show.blade.php
@@ -4,7 +4,13 @@
         <x-layout.general.breadcrumb-option name="Legitimaciones" arrow="true" :route="route('legitimation.index')" />
         <x-layout.general.breadcrumb-option :name="$event->name" arrow="false" />
     </x-layout.general.breadcrumbs>
+    <div
+        class="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-md text-red-100 bg-red-700 border border-red-700 mb-5 py-3">
+        <div class="text-xl max-w-full flex-initial font-bold text-center">
+            NADIE PODRÁ INICIAR CONTEO DE VOTOS, HASTA QUE SE LES COMUNIQUE POR PARTE DEL EQUIPO JURÍDICO QUE ESTARÁ
+            CONECTADO CON EL CENTRO, PARA DAR LA INDICACIÓN DE INICIO DEL eSCRUTINIO DE BOLETAS.</div>
 
+    </div>
     <div class="grid grid-cols-2 gap-5 mb-5">
         @if (Auth::user()->hasPermission('Administrator'))
         <x-card-image image="{{asset('svg/note.svg')}}">
