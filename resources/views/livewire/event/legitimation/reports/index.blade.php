0433aed74a9be3788497bf252f68e62daceaba96
ElSeoClub <elseoclub@gmail.com>
2021-12-01 03:06:51 -0600

    error en semaforo

--- resources/views/livewire/event/legitimation/reports/index.blade.php
+++ resources/views/livewire/event/legitimation/reports/index.blade.php
@@ -66,7 +66,7 @@
             <div class="flex gap-5 flex-wrap">
                 @foreach ($locations as $location)
                 <div
-                    class="w-12 h-12 text-center bg-{{$location->apertura != null ? 'green':'gray'}}-600 text-white rounded-full flex content-center flex-wrap justify-center font-bold text-xl">
+                    class="w-12 h-12 text-center bg-{{$location->cierre != null ? 'green':'gray'}}-600 text-white rounded-full flex content-center flex-wrap justify-center font-bold text-xl">
                     <div>{{$location->name}}</div>
                 </div>
                 @endforeach
@@ -77,7 +77,7 @@
             <div class="flex gap-5 flex-wrap">
                 @foreach ($locations as $location)
                 <div
-                    class="w-12 h-12 text-center bg-{{$location->apertura != null ? 'green':'gray'}}-600 text-white rounded-full flex content-center flex-wrap justify-center font-bold text-xl">
+                    class="w-12 h-12 text-center bg-{{$location->conteo != null ? 'green':'gray'}}-600 text-white rounded-full flex content-center flex-wrap justify-center font-bold text-xl">
                     <div>{{$location->name}}</div>
                 </div>
                 @endforeach
