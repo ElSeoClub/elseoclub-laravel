37f1832ffff36438edc7556c1fac8f1dec1e0916
ElSeoClub <elseoclub@gmail.com>
2021-12-01 04:29:03 -0600

    test

--- resources/views/livewire/event/legitimation/reports/index.blade.php
+++ resources/views/livewire/event/legitimation/reports/index.blade.php
@@ -2,35 +2,35 @@
     <div class="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 px-4 mb-5">
         <!-- SMALL CARD ROUNDED -->
         <div wire:click="display('status')"
-            class="bg-{{$view == 'status' ? 'red-50':'gray-100'}} border-{{$view == 'status' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
+            class="bg-{{$view == 'status' ? 'red-50':'gray-100'}} border-{{$view == 'status' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-between cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
             <img class="w-16 h-16 object-cover" src="{{asset('svg/status.svg')}}" alt="" />
             <div class="flex flex-col justify-center">
                 <p class="text-gray-900 dark:text-gray-300 font-semibold text-xl">Estado de las sedes</p>
             </div>
         </div>
         <div wire:click="display('attendance')"
-            class="bg-{{$view == 'attendance' ? 'red-50':'gray-100'}} border-{{$view == 'attendance' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
+            class="bg-{{$view == 'attendance' ? 'red-50':'gray-100'}} border-{{$view == 'attendance' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-between cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
             <img class="w-16 h-16 object-cover" src="{{asset('svg/attendant-list.svg')}}" alt="" />
             <div class="flex flex-col justify-center">
                 <p class="text-gray-900 dark:text-gray-300 font-semibold text-xl">Asistencia de las sedes</p>
             </div>
         </div>
         <div wire:click="display('attendance2')"
-            class="bg-{{$view == 'attendance2' ? 'red-50':'gray-100'}} border-{{$view == 'attendance2' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
+            class="bg-{{$view == 'attendance2' ? 'red-50':'gray-100'}} border-{{$view == 'attendance2' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-between cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
             <img class="w-16 h-16 object-cover" src="{{asset('svg/organization.svg')}}" alt="" />
             <div class="flex flex-col justify-center">
                 <p class="text-gray-900 dark:text-gray-300 font-semibold text-xl">Asistencia por coordinación</p>
             </div>
         </div>
         <div wire:click="display('count')"
-            class="bg-{{$view == 'count' ? 'red-50':'gray-100'}} border-{{$view == 'count' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
+            class="bg-{{$view == 'count' ? 'red-50':'gray-100'}} border-{{$view == 'count' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-between cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
             <img class="w-16 h-16 object-cover" src="{{asset('svg/results.svg')}}" alt="" />
             <div class="flex flex-col justify-center">
                 <p class="text-gray-900 dark:text-gray-300 font-semibold text-xl">Preliminar de votaciones por sede</p>
             </div>
         </div>
         <div wire:click="display('count2')"
-            class="bg-{{$view == 'count2' ? 'red-50':'gray-100'}} border-{{$view == 'count2' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
+            class="bg-{{$view == 'count2' ? 'red-50':'gray-100'}} border-{{$view == 'count2' ? 'red-900':'gray-600'}} bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | flex justify-between cursor-pointer | hover:bg-gray-200  hover:border-gray-700 | transition-colors duration-500">
             <img class="w-16 h-16 object-cover" src="{{asset('svg/results.svg')}}" alt="" />
             <div class="flex flex-col justify-center">
                 <p class="text-gray-900 dark:text-gray-300 font-semibold text-xl">Preliminar de votaciones por
