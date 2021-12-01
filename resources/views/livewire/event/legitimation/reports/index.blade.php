69ee4456e741393807a15f44f2fbc1c3e3bfc9c4
ElSeoClub <elseoclub@gmail.com>
2021-12-01 02:25:55 -0600

    orden

--- resources/views/livewire/event/legitimation/reports/index.blade.php
+++ resources/views/livewire/event/legitimation/reports/index.blade.php
@@ -1,6 +1,6 @@
 <x-card>
     <div class="flex gap-5 flex-wrap">
-        @foreach ($event->locations as $location)
+        @foreach ($locations as $location)
         <div
             class="w-12 h-12 text-center bg-gray-400 rounded-full flex content-center flex-wrap justify-center font-bold text-xl">
             <div>{{$location->name}}</div>
