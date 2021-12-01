33290485320922e843dbaf56afa1352c64b9fc84
ElSeoClub <elseoclub@gmail.com>
2021-12-01 02:30:25 -0600

    semaforo llegada

--- resources/views/livewire/event/legitimation/reports/index.blade.php
+++ resources/views/livewire/event/legitimation/reports/index.blade.php
@@ -2,7 +2,7 @@
     <div class="flex gap-5 flex-wrap">
         @foreach ($locations as $location)
         <div
-            class="w-12 h-12 text-center bg-gray-400 rounded-full flex content-center flex-wrap justify-center font-bold text-xl">
+            class="w-12 h-12 text-center bg-{{$location->llegada_verificador != null ? 'green':'gray'}}-600 text-white rounded-full flex content-center flex-wrap justify-center font-bold text-xl">
             <div>{{$location->name}}</div>
         </div>
         @endforeach
