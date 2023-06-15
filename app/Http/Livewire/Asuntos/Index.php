36d119f2ac375b2595d1cbf06bdb31196c680b63
ElSeoClub <elseoclub@gmail.com>
2023-06-15 11:16:56 -0600

    Se agrega buscador de casos Se muestran solo casos abiertos

--- app/Http/Livewire/Asuntos/Index.php
+++ app/Http/Livewire/Asuntos/Index.php
@@ -5,13 +5,17 @@ namespace App\Http\Livewire\Asuntos;
 use App\Models\Asunto;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    public $search = '';
 
     public function render()
     {
-        $asuntos = Asunto::where('user_id',Auth::user()->id)->get();
+        $asuntos = Asunto::where('user_id',Auth::user()->id)->where('status','abierto')->where('expediente','like','%'.$this->search.'%')->paginate(10);
         return view('livewire.asuntos.index', compact('asuntos'));
     }
 }
--- resources/views/livewire/asuntos/index.blade.php
+++ resources/views/livewire/asuntos/index.blade.php
@@ -1,4 +1,5 @@
 <section class="flex flex-col gap-5">
+    <x-search></x-search>
     <ul class="shadow rounded divide-y bg-white">
         @foreach($asuntos as $asunto)
             <a href="{{route('asuntos.asunto',['asunto' => $asunto])}}" class="p-5 hover:bg-gray-50 flex w-full gap-3 hover:bg-gray-50 cursor-pointer">
@@ -6,6 +7,7 @@
             </a>
         @endforeach
     </ul>
+    {{$asuntos->links()}}
     <x-button :href="route('asuntos.crear')" color="red" icon="fas fa-plus">Nuevo asunto</x-button>
 </section>
 
