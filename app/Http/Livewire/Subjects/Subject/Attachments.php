27198e8059b435d3e6a68b6eef4f0acff6158613
ElSeoClub <elseoclub@gmail.com>
2023-08-28 12:58:08 -0600

    Se agrega subir multiples documentos

--- app/Http/Livewire/Subjects/Subject/Attachments.php
+++ app/Http/Livewire/Subjects/Subject/Attachments.php
@@ -12,10 +12,10 @@ class Attachments extends Component
 {
     use WithFileUploads;
 
-    public $file;
-    public $fileName;
+    public $files;
     public $subject;
     public string $search = '';
+
 
 
     public function mount(Subject $subject){
@@ -24,38 +24,38 @@ class Attachments extends Component
 
     public function storeFile(){
         $this->validate([
-            'file'     => 'required',
-            'fileName' => 'required'
+            'files'     => 'required'
         ]);
         $ext = 'unf';
-        if ($this->file) {
-            $filePath = $this->file->store('archivo', ['disk' => 'public']);
-            $ext = explode('.', $filePath);
-            $ext = end($ext);
+
+        if($this->files){
+            foreach($this->files as $file){
+                if ($file) {
+                    $filePath = $file->store('archivo', ['disk' => 'public']);
+                    $ext = explode('.', $filePath);
+                    $ext = end($ext);
+                }
+
+                $this->subject->attachments()->create([
+                    'name' => $file->getClientOriginalName(),
+                    'extension' => $ext,
+                    'path' => $filePath,
+                    'user_id' => Auth::user()->id
+                ]);
+
+                Auth::user()->activities()->create([
+                    'type'     => 'digitalización',
+                    'comments' => 'Digitalización de documento: <a target="_blank" class="font-bold text-red-600" href="'.asset('storage/'.$filePath).'">'.$file->getClientOriginalName().'</a> en ' . $this->subject->matter->name . ': <a href="'.route('subjects.subject.attachments',$this->subject).'" class="font-bold text-red-600">' . $this->subject->name.'</a>.'
+                ]);
+            }
         }
-
-        $this->subject->attachments()->create([
-            'name' => $this->fileName,
-            'extension' => $ext,
-            'path' => $filePath,
-            'user_id' => Auth::user()->id
-        ]);
-
-        Auth::user()->activities()->create([
-            'type'     => 'digitalización',
-            'comments' => 'Digitalización de documento: <a target="_blank" class="font-bold text-red-600" href="'.asset('storage/'.$filePath).'">'.$this->fileName.'</a> en ' . $this->subject->matter->name . ': <a href="'.route('subjects.subject.attachments',$this->subject).'" class="font-bold text-red-600">' . $this->subject->name.'</a>.'
-        ]);
-        $this->file = null;
-        $this->fileName = null;
+        $this->files = null;
+        $this->emit('saveAlert', 'Archivos subidos exitosamente.');
     }
 
 
     public function render()
     {
-        if(isset($this->file)){
-            $this->fileName = $this->file->getClientOriginalName();
-        }
-
         $attachments = $this->subject->fresh()->attachments()->where('name','like','%'.$this->search.'%')->orderBy('name','asc')->get();
         return view('livewire.subjects.subject.attachments',compact('attachments'));
     }
--- resources/views/livewire/subjects/subject/attachments.blade.php
+++ resources/views/livewire/subjects/subject/attachments.blade.php
@@ -17,22 +17,15 @@
             >
                 <!-- File Input -->
                 <input class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-                       type="file" wire:model="file" enctype="multipart/form-data" />
+                       type="file" wire:model="files" enctype="multipart/form-data" multiple/>
                 
                 <!-- Progress Bar -->
                 <div x-show="isUploading">
                     <progress max="100" x-bind:value="progress" class="w-full"></progress>
                 </div>
             </div>
-            @if(isset($file))
-                @if(in_array($file->getClientOriginalExtension(),[   'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-                    'mov', 'avi', 'wmv', 'mp3', 'm4a',
-                    'jpg', 'jpeg', 'mpga', 'webp', 'wma',
-                ]))
-                    <img src="{{  $file->temporaryUrl() }}" width='200px;'>
-                @endif
-                <input type="text" wire:model.defer="fileName">
-                <x-button click="storeFile()">Guardar</x-button>
+            @if(isset($files))
+                <x-button click="storeFile()" color="green" class="full">Guardar</x-button>
                 <x-jet-input-error for="fileName"></x-jet-input-error>
             @endif
         </div>
