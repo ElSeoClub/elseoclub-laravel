55f99c85045f669c5d2f1eeec9be4a9ac38ee0cc
ElSeoClub <elseoclub@gmail.com>
2023-11-13 11:18:38 -0600

    Add screen

--- resources/views/livewire/event/legitimation/control/asistencia.blade.php
+++ resources/views/livewire/event/legitimation/control/asistencia.blade.php
@@ -2,12 +2,24 @@
     <div class="flex flex-row gap-3 w-full p-6 bg-gray-100">
         <div class="w-[750px]">
             <div class="bg-white grid grid-cols-12 w-[750px] text-2xl rounded border shadow">
-                <div class="col-span-9 px-3  pb-1 font-bold">Sedes</div>
+                <div class="col-span-9 px-3  pb-1 font-bold"></div>
                 <div class="col-span-3 px-3  pb-1 font-bold text-center">Asistentes</div>
                 @foreach($event->locations as $location)
                     <div class="col-span-9 px-3 pb-1 font-bold">{{$location->name}}</div>
-                    <div class="col-span-3 px-3  pb-1 font-bold text-center">{{$location->guests()->count()}} de {{$location->guests()->count()}}</div>
+                    <div class="col-span-3 px-3  pb-1 font-bold text-center">{{$location->guests()->whereNotNull('attendance_door_id')->count()}} de {{$location->guests()->count()}}</div>
                 @endforeach
+            </div>
+            <div class="w-full mt-5">
+                            <span class="font-bold text-xl">Asistencia general:
+                            {{$event->guests()->whereNotNull('attendance_door_id')->count()}} de
+                            {{$event->guests()->count()}}
+                            </span>
+                <div class=" shadow-md w-full bg-gray-200 h-9 rounded">
+                    <div class="bg-green-500 text-xs leading-none text-center text-white h-full rounded py-2 font-bold text-lg"
+                         style="width: {{round(($event->guests()->whereNotNull('attendance_door_id')->count()/$event->guests()->count())*100)}}%">
+                        {{round(($event->guests()->whereNotNull('attendance_door_id')->count()/$event->guests()->count())*100)}}%
+                    </div>
+                </div>
             </div>
         </div>
         <div class="grid grid-cols-5 items-center text-center overflow-y-scroll"  style="height:calc(100vh - 50px)">
