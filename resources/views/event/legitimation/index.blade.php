c1e5c299b91ed9f8659fbf1f725e15317b75e5e7
ElSeoClub <elseoclub@gmail.com>
2023-11-14 14:59:08 -0600

    Add screen

--- resources/views/event/legitimation/index.blade.php
+++ resources/views/event/legitimation/index.blade.php
@@ -1,7 +1,7 @@
 <x-general-layout>
-    <x-slot name="title">Legitimaciones</x-slot>
+    <x-slot name="title">Eventos</x-slot>
     <x-layout.general.breadcrumbs>
-        <x-layout.general.breadcrumb-option name="Legitimaciones" arrow="false" />
+        <x-layout.general.breadcrumb-option name="Eventos" arrow="false" />
     </x-layout.general.breadcrumbs>
     @livewire('event.legitimation.table')
 </x-general-layout>
