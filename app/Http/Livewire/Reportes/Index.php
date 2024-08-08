5d4fb941034db56ed8dd24c4ab4381af7bcf0735
ElSeoClub <elseoclub@gmail.com>
2024-08-08 12:56:16 -0600

    Remove test

--- app/Http/Livewire/Reportes/Index.php
+++ app/Http/Livewire/Reportes/Index.php
@@ -10,22 +10,6 @@ class Index extends Component
 {
     public function render()
     {
-        $proximoLunes = Carbon::now()->startOfWeek()->addWeek(); // Obtener la fecha del próximo lunes
-        $proximoDomingo = $proximoLunes->copy()->addDays(6); // Agregar 6 días para obtener la fecha del próximo domingo
-
-        $actuaciones = Task::whereBetween('fecha', [$proximoLunes, $proximoDomingo])
-                           ->whereHas('subject.matter', function($query) {
-                               $query->where('id', 24);
-                           })
-                           ->with('subject')
-                           ->orderBy('fecha', 'asc')
-                           ->get();
-
-        foreach($actuaciones as $n => $actuacion) {
-            if($n > 0) {
-                dd( unserialize( $actuacion->subject->metadata) );
-            }
-        }
         return view('livewire.reportes.index');
     }
 }
