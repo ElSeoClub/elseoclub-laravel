e437fb49add430743e466d68cd04fa4409571778
ElSeoClub <elseoclub@gmail.com>
2024-08-08 12:35:47 -0600

    Test metadata

--- app/Http/Livewire/Reportes/Index.php
+++ app/Http/Livewire/Reportes/Index.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Livewire\Reportes;
 
+use App\Models\Task;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
+        $proximoLunes = Carbon::now()->startOfWeek()->addWeek(); // Obtener la fecha del próximo lunes
+        $proximoDomingo = $proximoLunes->copy()->addDays(6); // Agregar 6 días para obtener la fecha del próximo domingo
+
+        $actuaciones = Task::whereBetween('fecha', [$proximoLunes, $proximoDomingo])
+                           ->whereHas('subject.matter', function($query) {
+                               $query->where('id', 24);
+                           })
+                           ->with('subject')
+                           ->orderBy('fecha', 'asc')
+                           ->get();
+
+        foreach($actuaciones as $actuacion) {
+            dd( unserialize( $actuacion->metadata ) );
+        }
         return view('livewire.reportes.index');
     }
 }
