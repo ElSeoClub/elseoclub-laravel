533fe0d9e06f61dd8384b4f8888c5bd6e5c2cac3
ricardo <elseoclub@gmail.com>
2025-04-03 17:51:13 -0600

    test

--- app/Http/Controllers/Api/StreetController.php
+++ app/Http/Controllers/Api/StreetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\History;
 use Illuminate\Http\Request;
 
 class StreetController extends Controller
@@ -11,6 +12,18 @@ class StreetController extends Controller
     {
         // Obtener el valor del parámetro 'd' de la solicitud
         $d = $request->query('d');
+
+        $pos = strpos($d, 'ID:');
+
+        // Extraer el dígito después de "ID:"
+        if ($pos !== false) {
+            $idValue = substr($d, $pos + 3, 1); // +3 para saltar "ID:" y tomar 1 carácter
+            History::create([
+                'oid' => $idValue,
+                'value' => $d,
+            ]);
+        } else {
+        }
 
         // Retornar una respuesta JSON con el valor de 'd'
         return response()->json([
